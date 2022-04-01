582d906872930f2c250831383bc9f4e708ca2184
DaRa133 <pr_rrom@mail.ru>
2022-04-01 08:39:17 +0300

    duplicateEncode.js completed

---
+++ duplicateEncode.js
@@ -0,0 +1,16 @@
+function duplicateEncode(str) {
+    str = str.toLowerCase();
+    str = str.split('');
+    var new_str = "";
+    
+    for (let i = 0; i < str.length; i++) {
+        if (str.indexOf(str[i])==str.lastIndexOf(str[i])) {
+           new_str+="(";
+        }
+        else    
+        {
+            new_str+=")";
+        }
+    }
+    console.log(new_str);
+}
